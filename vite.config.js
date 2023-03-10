b4ebaf87e0aad67d1d57ad0d9bd494319b8798f7
ahmed sameh <asameh1500@gmail.com>
2023-03-10 14:10:40 +0200

    vite

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,16 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+
+
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-})
+// export default defineConfig({
+//   plugins: [react()],
+//   appType: "mpa",
+// });
+
+import pluginRewriteAll from 'vite-plugin-rewrite-all';
+
+export default {
+  plugins: [pluginRewriteAll(), react()]
+}
